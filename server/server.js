1ed25f27b09ef710e529111617af4fb5a3dc1a57
Amanda Araskog <amanda.araskog@funnel.io>
2023-02-26 11:17:17 +0100

    Add endpoints for sign-in and registration

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.post("/signin", (req, res) => {
+  console.log(req);
+});
+
+app.post("/register", (req, res) => {
+  console.log(req);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
